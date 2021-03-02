45538141c9f54f2d239b3510b3fc9728d36c4985
Janine Birnbaum <30421133+JanineBirnbaum18@users.noreply.github.com>
2021-03-01 22:48:22 -0500

    Allow searching for different location
    Map centers on new location, suppress error of no events in catalog

--- recread.py
+++ recread.py
@@ -22,7 +22,7 @@ import pandas as pd
 
 from bokeh.layouts import column, row, widgetbox
 from bokeh.models import ColumnDataSource, CustomJS, Div, Slider, TextInput, Circle, HoverTool, TapTool, Select, RangeSlider
-from bokeh.models.widgets import Button
+from bokeh.models.widgets import Button, Panel, Tabs
 from bokeh.plotting import figure, curdoc
 from bokeh.tile_providers import get_provider, Vendors
 
@@ -93,7 +93,7 @@ for iev, event in enumerate(eventlist) :
     evlat = np.append(evlat,eventlist[iev].origins[0].latitude)
     evdepth = np.append(evdepth,eventlist[iev].origins[0].depth)
     evmag = np.append(evmag,eventlist[iev].magnitudes[0].mag)
-    evtime = np.append(evtime,eventlist[iev].origins[0].time.strftime("%d/%m/%y %H:%M"))
+    evtime = np.append(evtime,eventlist[iev].origins[0].time.strftime("%y/%m/%d %H:%M"))
     ID = np.append(ID,str(eventlist[iev].resource_id))
     
 # define coordinate transformations from lat/lon to Web Mercator
@@ -108,18 +108,26 @@ source = source = ColumnDataSource(data={'x': x1,'y': y1,'lat':evlat,'lon':evlon
 
 def update_search_params(attrname, old, new):
 
+    eventlist = []
+    
+    [x_cntr, y_cntr] = latlon2webmercator.transform(float(input_lat.value),float(input_lon.value))
+    
+    p.x_range.start = x_cntr-padding
+    p.x_range.end   = x_cntr+padding
+    p.y_range.start = y_cntr-padding
+    p.y_range.end   = y_cntr+padding
     # Get the current slider values
-    lat = input_lat.value
-    lon = input_lon.value
     t11 = UTCDateTime(input_time.value)
     t22 = t11 + 3600*search_time_range.value
 
     # Fetch data
     client = Client(input_webservice.value)
-    eventlist = (client.get_events(starttime=t11,endtime=t22,latitude=float(input_lat.value),longitude=float(input_lon.value),minradius=0,maxradius=float(search_rad.value),
-                 minmagnitude=float(input_mag.value[0]),maxmagnitude=float(input_mag.value[1]),magnitudetype=mag_type.value,catalog='GCMT',
-                 orderby='time-asc'))
     
+    try:
+        eventlist = (client.get_events(starttime=t11,endtime=t22,latitude=float(input_lat.value),longitude=float(input_lon.value),minradius=0,maxradius=float(search_rad.value),
+                minmagnitude=float(input_mag.value[0]),maxmagnitude=float(input_mag.value[1]),magnitudetype=mag_type.value,catalog='GCMT',
+                orderby='time-asc'))
+     
     # HELPFUL THINGS TO PRINT?
     # print(eventlist.__str__(print_all=True))
     # eventlist.plot(projection="global")
@@ -128,24 +136,29 @@ def update_search_params(attrname, old, new):
     # eventlist[0].event_descriptions has location description text
     # eventlist[0].magnitudes lists magnitude and magnitude type
     
-    evlon = np.array([])
-    evlat = np.array([])
-    evdepth = np.array([])
-    evmag = np.array([])
-    evtime = np.array([])
-    ID = np.array([])
+        evlon = np.array([])
+        evlat = np.array([])
+        evdepth = np.array([])
+        evmag = np.array([])
+        evtime = np.array([])
+        ID = np.array([])
 
-    for iev, event in enumerate(eventlist) :
-        evlon = np.append(evlon,eventlist[iev].origins[0].longitude)
-        evlat = np.append(evlat,eventlist[iev].origins[0].latitude)
-        evdepth = np.append(evdepth,eventlist[iev].origins[0].depth)
-        evmag = np.append(evmag,eventlist[iev].magnitudes[0].mag)
-        evtime = np.append(evtime,eventlist[iev].origins[0].time.strftime("%d/%m/%y %H:%M"))
-        ID = np.append(ID,str(eventlist[iev].resource_id))
+        for iev, event in enumerate(eventlist) :
+            evlon = np.append(evlon,eventlist[iev].origins[0].longitude)
+            evlat = np.append(evlat,eventlist[iev].origins[0].latitude)
+            evdepth = np.append(evdepth,eventlist[iev].origins[0].depth)
+            evmag = np.append(evmag,eventlist[iev].magnitudes[0].mag)
+            evtime = np.append(evtime,eventlist[iev].origins[0].time.strftime("%y/%m/%d %H:%M"))
+            ID = np.append(ID,str(eventlist[iev].resource_id))
 
-    [x1, y1] = latlon2webmercator.transform(evlat,evlon) # transform event coordinates  
-    source.data = {'x': x1,'y': y1,'lat':evlat,'lon':evlon,'depth':evdepth/1000,
-				'mag':evmag,'size':4*evmag,'time':evtime, 'id':ID}
+        [x1, y1] = latlon2webmercator.transform(evlat,evlon) # transform event coordinates  
+        source.data = {'x': x1,'y': y1,'lat':evlat,'lon':evlon,'depth':evdepth/1000,
+                       'mag':evmag,'size':4*evmag,'time':evtime, 'id':ID}
+    
+    except Exception as e:
+        print(e)
+        source.data = {'x':[],'y': [],'lat':[],'lon':[],'depth':[],
+				'mag':[],'size':[],'time':[], 'id':[]}
 
 for w in [input_lat, input_lon, input_time, input_mag, mag_type, search_rad, input_webservice, input_ID]:
     w.on_change('value', update_search_params)
@@ -179,7 +192,7 @@ p.add_tools(HoverTool(tooltips=TOOLTIPS, callback=callbackhover, renderers=[cr],
 
 # Save event info
 def save_button_callback():
-    eventdat = pd.Series(data={'ID':input_ID.value,'lat':input_lat.value, 'lon':input_lon.value, 'time':input_time.value, 'mag':input_mag.value})
+    eventdat = pd.Series(data={'ID':input_ID.value,'lat':input_lat.value, 'lon':input_lon.value, 'time':input_time.value, 'mag':input_mag.value[0]})
     eventdat.to_hdf('eventdat',key='meta')
 
 save_button = Button(label='Save', button_type='success')
@@ -193,6 +206,19 @@ quit_button = Button(label="Quit", button_type="success")
 quit_button.on_click(quit_button_callback)
 
 # Display plots
+layout1 = column(Div(text='<h1> Select event <h1>'),
+                 (row(column([input_lat,
+                              input_lon,
+                              search_rad,
+                              input_time,
+                              search_time_range,
+                              input_mag,mag_type,
+                              input_webservice,
+                              save_button,
+                              quit_button]),p)))
+panel1 = Panel(child=layout1, title='Select event')
+tabs = Tabs(tabs=[panel1])
+
 bokeh_doc = curdoc()
-bokeh_doc.add_root(column(Div(text='<h1> Select event <h1>'),(row(column([input_lat,input_lon,search_rad,input_time,search_time_range,input_mag,mag_type,input_webservice,save_button,quit_button]),p))))
-bokeh_doc.title = "Select Event"
+bokeh_doc.add_root(tabs)
+bokeh_doc.title = "Record Reading Server App"
