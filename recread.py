72794a0016cdeb7b7af12fc49ca55d621f9e7e85
Janine Birnbaum <30421133+JanineBirnbaum18@users.noreply.github.com>
2021-03-03 00:16:39 -0500

    Plot station map, layout for download params
    Interactive station map, layout for widgets to set download parameters. Does not yet download waveforms.

--- recread.py
+++ recread.py
@@ -1,29 +1,41 @@
 ''' Record Reading Plots
-Program to extract and plot global waveforms for a specified seismic event and maps regional focal mechanisms. This program creates an interactive plot to view global waveforms for a user-specified seismic event. The program also has options to plot regional maps of focal mechanisms from the CMT catalog (globalcmt.org).
-This is a python adaptation to the original record reading waveform plot program written by Ge Jin (jinwar@gmail.com) in Matlab.
-Contributors: Janine Birnbaum, Theresa Sawi, Christopher Carchedi, and Michelle Lee
+Program to extract and plot global waveforms for a specified seismic event and 
+maps regional focal mechanisms. This program creates an interactive plot to 
+view global waveforms for a user-specified seismic event. The program also has 
+options to plot regional maps of focal mechanisms from the CMT catalog 
+globalcmt.org).
+This is a python adaptation to the original record reading waveform plot 
+program written by Ge Jin (jinwar@gmail.com) in Matlab.
+Contributors: Janine Birnbaum, Theresa Sawi, Christopher Carchedi, and Michelle 
+Lee
 
 Instructions:
-1. Event Selection: Select event to plot global waveforms for by inputting location and magnitude parameters of desired event
+1. Event Selection: Select event to plot global waveforms for by inputting 
+location and magnitude parameters of desired event
 2. (User steps to download the data?)
-3. Plotting the waveforms: The global waveforms will be be displayed on an interactive plot allowing users to zoom in/out, change component, change frequency band, ... (and other features)
+3. Plotting the waveforms: The global waveforms will be be displayed on an 
+interactive plot allowing users to zoom in/out, change component, change 
+frequency band, ... (and other features)
 4. Plotting focal mechanisms: (info on what user needs to do for this)
 
 Use the ``bokeh serve`` command to run the example by executing:
-    bokeh serve recread.py
+    bokeh serve --show recread.py
 at your command prompt. Then navigate to the URL
     http://localhost:5006/recread
 in your browser.
 '''
 
+###############################################################################
 # Imports
+###############################################################################
 import sys
 
 import numpy as np
 import pandas as pd
 
 from bokeh.layouts import column, row
-from bokeh.models import ColumnDataSource, CustomJS, Div, OpenURL, Slider, TextInput, HoverTool, TapTool, Select, RangeSlider
+from bokeh.models import ColumnDataSource, CustomJS, Div, OpenURL, Slider
+from bokeh.models import TextInput, HoverTool, TapTool, Select, RangeSlider, MultiChoice, RadioButtonGroup
 from bokeh.models.widgets import Button, Panel, Tabs
 from bokeh.plotting import figure, curdoc
 from bokeh.tile_providers import get_provider
@@ -36,6 +48,9 @@ from obspy.io.sac import SACTrace
 
 from pyproj import Transformer
 
+###############################################################################
+# Tab 1: Select event
+###############################################################################
 # load initial inputs from file
 meta = pd.read_hdf('eventdat',key='meta')
 
@@ -53,10 +68,10 @@ input_lon = TextInput(value=str(lon), title='Longitude (-180 to 180):')
 input_time = TextInput(value=meta.time[0],title='Start time UTC (YYYY-MM-DD HH:MM:SS)')
 search_time_range = Slider(start=0,end=72,value=72, title='Search time range (hrs)')
 input_mag = RangeSlider(start=0,end=9,value=(meta.mag[0]-0.5,meta.mag[0]+0.5),title='Magnitude') # Can probably add an if condition to change the slider bar boundaries for other magnitude scales
-mag_type = Select(title="Magnitude type", value="Mw",
+mag_type = Select(title='Magnitude type', value='Mw',
                options=['Mw'])
 search_rad = Slider(start=0,end=20,value=10,title='Search radius (deg)')
-input_webservice = Select(title="Catalog", value='IRIS',
+input_webservice = Select(title='Catalog', value='IRIS',
                options=['IRIS'])
 input_ID = TextInput(value=str(ID[0]))
 
@@ -65,8 +80,8 @@ latlon2webmercator = Transformer.from_crs('EPSG:4326', 'EPSG:3857')
 webmercator2latlon = Transformer.from_crs('EPSG:3857', 'EPSG:4326')
 
 [x0, y0] = latlon2webmercator.transform(lat, lon) # transform search coordinate for plotting  
-xcoord = TextInput(value='0')
-ycoord = TextInput(value='0')
+xcoord = TextInput(value=str(x0))
+ycoord = TextInput(value=str(y0))
 
 # Fetch initital data
 evlon = np.array([])
@@ -90,7 +105,7 @@ try:
         evlat = np.append(evlat,eventlist[iev].origins[0].latitude)
         evdepth = np.append(evdepth,eventlist[iev].origins[0].depth)
         evmag = np.append(evmag,eventlist[iev].magnitudes[0].mag)
-        evtime = np.append(evtime,eventlist[iev].origins[0].time.strftime("%y/%m/%d %H:%M"))
+        evtime = np.append(evtime,eventlist[iev].origins[0].time.strftime('%y/%m/%d %H:%M'))
         ID = np.append(ID,str(eventlist[iev].resource_id))
 
     # plot initial data
@@ -137,7 +152,7 @@ def update_search_params(attrname, old, new):
             evlat = np.append(evlat,eventlist[iev].origins[0].latitude)
             evdepth = np.append(evdepth,eventlist[iev].origins[0].depth)
             evmag = np.append(evmag,eventlist[iev].magnitudes[0].mag)
-            evtime = np.append(evtime,eventlist[iev].origins[0].time.strftime("%y/%m/%d %H:%M"))
+            evtime = np.append(evtime,eventlist[iev].origins[0].time.strftime('%y/%m/%d %H:%M'))
             ID = np.append(ID,str(eventlist[iev].resource_id))
 
         [x1, y1] = latlon2webmercator.transform(evlat,evlon) # transform event coordinates  
@@ -158,7 +173,7 @@ for w in [input_lat, input_lon, input_time, input_mag, mag_type, search_rad, inp
 # figure bounds supplied in web mercator coordinates
 padding = 0.5*10**6 # about 5 degrees
 p = figure(x_range=(x0-padding, x0+padding), y_range=(y0-padding, y0+padding),
-                x_axis_type="mercator", y_axis_type="mercator",tools='tap')
+                x_axis_type='mercator', y_axis_type='mercator',tools='tap')
 p.add_tile(get_provider('ESRI_IMAGERY'))
    
 
@@ -216,10 +231,10 @@ def callbacktap2(attrname, old, new):
         input_time.value = str(eventlist[0].origins[0].time).split('.')[0].replace('T', ' ')
         input_mag.value  = (eventlist[0].magnitudes[0].mag - 0.1,eventlist[0].magnitudes[0].mag + 0.1)
         input_ID.value   = str(eventlist[0].resource_id)
-        evtime = eventlist[iev].origins[0].time.strftime("%y/%m/%d %H:%M")
+        evtime = eventlist[iev].origins[0].time.strftime('%y/%m/%d %H:%M')
         
-        source.data = {'x': xcoord.value,'y': ycoord.value,'lat':lat,'lon':lon,'depth':eventlist[0].origins[0].depth/1000,
-                       'mag':eventlist[0].magnitudes[0].mag,'size':4*eventlist[0].magnitudes[0].mag,'time':evtime, 'id':input_ID.value}
+        source.data = {'x': [float(xcoord.value)],'y': [float(ycoord.value)],'lat':[lat],'lon':[lon],'depth':[eventlist[0].origins[0].depth/1000],
+                       'mag':[eventlist[0].magnitudes[0].mag],'size':[4*eventlist[0].magnitudes[0].mag],'time':[evtime], 'id':[input_ID.value]}
     except Exception as e: 
         print(e)     
 
@@ -228,7 +243,7 @@ ycoord.on_change('value', callbacktap2)
     
 # Save event info
 def save_button_callback():
-    eventdat = pd.DataFrame(data={'ID':input_ID.value,'lat':input_lat.value, 'lon':input_lon.value, 'time':str(input_time.value), 'mag':input_mag.value[0]}, index=[0])
+    eventdat = pd.DataFrame(data={'ID':str(input_ID.value),'lat':float(input_lat.value), 'lon':float(input_lon.value), 'time':str(input_time.value), 'mag':float(input_mag.value[0])}, index=[0])
     eventdat.to_hdf('eventdat',key='meta')
 
 save_button = Button(label='Save', button_type='success')
@@ -238,7 +253,7 @@ save_button.on_click(save_button_callback)
 def quit_button_callback():
     sys.exit()  # Stop the server
     
-quit_button = Button(label="Quit", button_type="success")
+quit_button = Button(label='Quit', button_type='success')
 quit_button.on_click(quit_button_callback)
 
 # figure caption
@@ -257,8 +272,136 @@ layout1 = column(Div(text='<h1> Select event <h1>'),
                               figure1,
                               quit_button]),p)))
 panel1 = Panel(child=layout1, title='Select event')
-tabs = Tabs(tabs=[panel1])
+
+###############################################################################
+# Tab 2: Download data
+###############################################################################
+network_options = [('1', '_US-ALL'), ('2', '_GSN'), ('3', 'TA')]
+network = MultiChoice(title='Station network', value=['1', '2'],
+               options=network_options)
+add_network_text = TextInput(value='')
+add_network_button = Button(label='Add network',button_type='success')
+
+epicentral_distance = RangeSlider(start=0,end=180,value=(0,180),title='Epicentral distance (deg)')
+
+min_before = TextInput(value='10',title='Minutes before:')
+min_after = TextInput(value='120',title='Minutes after:')
+
+unit_options = ['Frequncy (Hz)', 'Period (s)']
+radio_button_group = RadioButtonGroup(labels=unit_options, active=1)
+lowfilter_left = TextInput(value='200')
+lowfilter_right = TextInput(value='30')
+midfilter_left = TextInput(value='25')
+midfilter_right = TextInput(value='5')
+highfilter_left = TextInput(value='5')
+highfilter_right = TextInput(value='2')
+resample_delta = TextInput(value='0.1',title='Resample rate:')
+
+load_stations = Button(label='Load station map', button_type='success')
+download_data = Button(label='Download data', button_type='success')
+
+stations_source = ColumnDataSource(data={'stat_x': [],'stat_y': [],
+                                             'stat_lat':[],'stat_lon':[],
+                                             'stat_name':[]})
+
+
+# download some station info based on networks
+# Stations map (could maybe pull station info from the networks and allow a select tool on a map)
+def add_network_option():
+    network_options.append((str(len(network_options)+1),str(add_network_text.value)))
+    network.options = network_options
+    network.value.append(str(len(network_options)))
+
+add_network_button.on_click(add_network_option)
+
+def networkscallback(attrs,new,old):
+    networks = network.value
+network.on_change('value', networkscallback)
+
+def select_by_dist(attrs,new,old):
+    xpadding = latlon2webmercator.transform(0,180)[0]/180*float(epicentral_distance.value[1])
+    ypadding = latlon2webmercator.transform(85,0)[1]/90*float(epicentral_distance.value[1])
+    p2.x_range.start=x_cntr-xpadding
+    p2.x_range.end  =x_cntr+xpadding
+    p2.y_range.start=np.max([y_cntr-ypadding,latlon2webmercator.transform(-85,0)[1]])
+    p2.y_range.end  =np.min([y_cntr+ypadding,latlon2webmercator.transform(85,0)[1]])
+
+x_cntr = float(xcoord.value)
+y_cntr = float(ycoord.value)
+xpadding = latlon2webmercator.transform(0,180)[0]/180*float(epicentral_distance.value[1])
+ypadding = latlon2webmercator.transform(85,0)[1]/90*float(epicentral_distance.value[1])
+
+p2 = figure(x_range=(x_cntr-xpadding, x_cntr+xpadding), 
+           y_range=(np.max([y_cntr-ypadding,latlon2webmercator.transform(-85,0)[1]]), np.min([y_cntr+ypadding,latlon2webmercator.transform(85,0)[1]])),
+           x_axis_type='mercator', y_axis_type='mercator',
+           tools='pan,wheel_zoom,box_zoom,reset',
+           active_drag='pan',active_scroll='wheel_zoom')
+
+p2.circle('x', 'y', size='size',source=source,color='red',line_color='black')
+p2.add_tile(get_provider('ESRI_IMAGERY'))
+
+epicentral_distance.on_change('value',select_by_dist)
+
+def load_stations_callback():
+    stat_lat = np.array([])
+    stat_lon = np.array([])
+    stat_name = np.array([])
+    t11 = UTCDateTime(str(input_time.value).replace('-','').replace(' ','').replace(':',''))
+    for opt in network.value:
+        try:
+            inventory = client.get_stations(network=str(network_options[int(opt)-1][1]), 
+                                        station="*",
+                                        latitude=float(input_lat.value), 
+                                        longitude=float(input_lon.value),
+                                        minradius=float(epicentral_distance.value[0]), 
+                                        maxradius=float(epicentral_distance.value[1]),
+                                        starttime=t11-float(min_before.value)*60,
+                                        endtime=t11+float(min_after.value)*60)
+            for net in inventory.networks:
+                for station in net.stations:
+                    stat_lat = np.append(stat_lat,station.latitude)
+                    stat_lon = np.append(stat_lon,station.longitude)
+                    stat_name = np.append(stat_name,station.code)
+                
+        except Exception as e: 
+            print(e)   
+    try:
+        [stat_x,stat_y] = latlon2webmercator.transform(stat_lat,stat_lon)
+        stat_x[stat_x<(x_cntr-latlon2webmercator.transform(0,180)[0])] += 2*latlon2webmercator.transform(0,180)[0]
+        stat_x[stat_x>(x_cntr+latlon2webmercator.transform(0,180)[0])] -= 2*latlon2webmercator.transform(0,180)[0]
+    except: 
+        stat_x = np.array([])
+        stat_y = np.array([])
+    stations_source.data = {'stat_x': stat_x,'stat_y': stat_y,
+                            'stat_lat':stat_lat,'stat_lon':stat_lon,
+                            'stat_name':stat_name}
+            
+load_stations.on_click(load_stations_callback)
+p2.triangle('stat_x', 'stat_y', size=10,source=stations_source,color='red',line_color='black')
+
+layout2 = row(column(Div(text='<h1>Downloading the data parameters<h1>'),
+                     network,
+                     row(add_network_text,add_network_button),
+                     epicentral_distance,
+                     Div(text='Download waveform length:'),
+                     row(min_before, min_after),
+                     Div(text='Waveform processing parameters:'),
+                     radio_button_group,
+                     Div(text='Low filter:'),
+                     row(lowfilter_left,lowfilter_right),
+                     Div(text='Mid filter:'),
+                     row(midfilter_left,midfilter_right),
+                     Div(text='High filter:'),
+                     row(highfilter_left,highfilter_right),
+                     resample_delta),
+                                                             column(p2,Div(text='Figure 2: Map of selected stations and event. Move the slider to change the radius from the event.'),load_stations,download_data))
+panel2 = Panel(child=layout2,title='Download data')
+
+###############################################################################
+# Compile tabs
+###############################################################################
+tabs = Tabs(tabs=[panel1, panel2])
 
 bokeh_doc = curdoc()
 bokeh_doc.add_root(tabs)
-bokeh_doc.title = "Record Reading Server App"
+bokeh_doc.title = 'Record Reading Server App'
